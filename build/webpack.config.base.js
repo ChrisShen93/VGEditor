51db599cfbf7a17f9785171ebcdf30c8faec5709
ChrisShen93 <chrisshen93@gmail.com>
2019-09-16 09:54:10 +0800

    fix: babel config for path alias

--- build/webpack.config.base.js
+++ build/webpack.config.base.js
@@ -1,4 +1,3 @@
-const path = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
@@ -11,11 +10,6 @@ module.exports = {
     ]
   },
   resolve: {
-    alias: {
-      '@components': path.resolve(__dirname, '../src/components'),
-      '@common': path.resolve(__dirname, '../src/common'),
-      '@utils': path.resolve(__dirname, '../src/utils')
-    },
     extensions: ['.js', '.vue']
   },
   plugins: [
