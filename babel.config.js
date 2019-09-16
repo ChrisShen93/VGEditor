045ff0e720da591f10cfbc4e12080fcf80db0b2a
ChrisShen93 <chrisshen93@gmail.com>
2019-09-16 09:54:10 +0800

    fix: babel config for path alias

--- babel.config.js
+++ babel.config.js
@@ -3,5 +3,16 @@ module.exports = {
     '@vue/app'
   ],
   plugins: [
+    '@babel/plugin-transform-runtime',
+    [
+      'module-resolver',
+      {
+        'alias': {
+          '@components': './src/components',
+          '@common': './src/common',
+          '@utils': './src/utils'
+        }
+      }
+    ]
   ]
 }
