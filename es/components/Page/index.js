66863908ab435191c31f768e6d726c80d5ea82ca
ChrisShen93 <chrisshen93@gmail.com>
2019-09-24 10:39:46 +0800

    build bundle

--- es/components/Page/index.js
+++ es/components/Page/index.js
@@ -8,10 +8,10 @@ export default {
   mounted: function mounted() {
     this.init().then(this.bindEvent);
   },
-  beforeDestroy: function beforeDestroy() {
-    this.page && this.page.destroy();
-    this.page = null;
-  },
+  // beforeDestroy () {
+  //   this.page && this.page.destroy()
+  //   this.page = null
+  // },
   methods: {
     getPageId: function getPageId() {// should be extend
     },
