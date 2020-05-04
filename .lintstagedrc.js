9a55ffb1ed00d92bc613b4f63fb33cee3526fbf1
ChrisShen93 <chrisshen93@gmail.com>
2020-05-04 08:28:51 +0800

    chore(lint-staged): remove 'git add' in tasks
    From v10 onwards the 'git add' part is automatic and not necessary to include in your configuration.

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '**/!templates/*.ts': ['npm run format', 'git add'],
-  '**/!templates/*.js': ['npm run format', 'git add'],
-  '*.md': ['prettier --parser markdown --write', 'git add'],
+  '**/!templates/*.ts': ['npm run format'],
+  '**/!templates/*.js': ['npm run format'],
+  '*.md': ['prettier --parser markdown --write'],
 };
